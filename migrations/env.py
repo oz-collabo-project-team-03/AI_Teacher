f461887cad525032a3c872d9bf2c96530d495ea0
hwangtate <ghkdxodud10253504@gmail.com>
2024-11-12 19:27:10 +0900

    fix: mypy 에러 수정

--- migrations/env.py
+++ migrations/env.py
@@ -15,6 +15,10 @@ load_dotenv()
 config = context.config
 
 database_url = os.environ.get("PG_DATABASE_URL")
+
+if database_url is None:
+    raise ValueError("Database URL environment variable 'PG_DATABASE_URL' is not set. ")
+
 config.set_main_option("sqlalchemy.url", database_url)
 
 if config.config_file_name is not None:
@@ -44,8 +48,13 @@ def do_run_migrations(connection):
 
 
 async def run_async_migrations() -> None:
+    config_section = config.get_section(config.config_ini_section)
+
+    if config_section is None:
+        raise ValueError(f"Configuration section {config.config_ini_section} not found")
+
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section),
+        config_section,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
