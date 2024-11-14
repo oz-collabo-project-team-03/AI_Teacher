d24229d87a8587abcd3701d4153bb58ac92221f6
skwwnl <skwwnl@gmail.com>
2024-11-15 00:59:03 +0900

    [Feature/Test] Add User async pytest & fix import declarative_base

--- src/config/database/postgresql.py
+++ src/config/database/postgresql.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 load_dotenv()
 
