995638eee7310a91dfe21d1dad5511bff4fff1ee
skwwnl <skwwnl@gmail.com>
2024-11-19 23:50:34 +0900

    [Feature/chat] Add Field in room entity

--- src/app/v1/chat/entity/room.py
+++ src/app/v1/chat/entity/room.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import BigInteger, DateTime, String, func
+from sqlalchemy import BigInteger, Boolean, DateTime, String, false, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.config.database import Base
@@ -10,6 +10,7 @@ class Room(Base):
     __tablename__ = "rooms"
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
+    help_checked: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
     last_message: Mapped[str] = mapped_column(String(300), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
