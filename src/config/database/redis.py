707e2326dc063daa997f573a1bb02850f3927f78
hyo00000 <gywkdk80@gmail.com>
2024-11-25 18:17:24 +0900

    [Feature/login]mypy 수정

--- src/config/database/redis.py
+++ src/config/database/redis.py
@@ -10,7 +10,7 @@ REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", "6379"))
 REDIS_DB_CACHE = int(os.environ.get("REDIS_DB_CACHE", "0"))
 
-redis_cache: Optional[Redis] = Redis(
+redis_cache: Redis = Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
     db=REDIS_DB_CACHE,
