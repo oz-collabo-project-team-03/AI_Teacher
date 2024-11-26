9756d6cf904b7e9d4a21654e82d2e9cadedbc77e
hyo00000 <gywkdk80@gmail.com>
2024-11-26 15:51:42 +0900

    [Feature/login]migrations추가

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,5 @@
+# type: ignore
+
 import asyncio
 import os
 from logging.config import fileConfig
@@ -9,31 +11,29 @@ from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 # 모델 import
-from src.config.database import Base
+from src.config.database import Base, database_models
 
 load_dotenv()
 
-user = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASSWORD")
-host = os.environ.get("DB_HOST")
-database = os.environ.get("DB_NAME")
-port = int(os.environ.get("DB_PORT", "5432"))
-
 config = context.config
 
-database_url = sqlalchemy.engine.URL.create(
-    drivername="postgresql+asyncpg",
-    username=user,
-    password=password,
-    host=host,
-    port=port,
-    database=database,
-)
+# user = os.environ.get("DB_USER")
+# password = os.environ.get("DB_PASSWORD")
+# host = os.environ.get("DB_HOST")
+# database = os.environ.get("DB_NAME")
+# port = int(os.environ.get("DB_PORT", "5432"))
+#
+# database_url = sqlalchemy.engine.URL.create(
+#     drivername="postgresql+asyncpg",
+#     username=user,
+#     password=password,
+#     host=host,
+#     port=port,
+#     database=database,
+# )
 
-if database_url is None:
-    raise ValueError("Database URL environment variable 'PG_DATABASE_URL' is not set. ")
-
-config.set_main_option("sqlalchemy.url", str(database_url))
+database_url = os.environ.get("PG_DATABASE_URL")
+config.set_main_option("sqlalchemy.url", database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
@@ -62,13 +62,8 @@ def do_run_migrations(connection):
 
 
 async def run_async_migrations() -> None:
-    config_section = config.get_section(config.config_ini_section)
-
-    if config_section is None:
-        raise ValueError(f"Configuration section {config.config_ini_section} not found")
-
     connectable = async_engine_from_config(
-        config_section,
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
