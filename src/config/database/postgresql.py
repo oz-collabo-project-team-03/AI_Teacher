515f2a43177a99197a055fef38ee50ef839f46de
skwwnl <skwwnl@gmail.com>
2024-12-11 03:53:55 +0900

    Feature/Chat CD

--- src/config/database/postgresql.py
+++ src/config/database/postgresql.py
@@ -1,9 +1,7 @@
 import os
 
-import sqlalchemy
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import (
-    AsyncEngine,
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
@@ -12,11 +10,6 @@ from sqlalchemy.orm import declarative_base
 
 load_dotenv()
 
-# user = os.environ.get("DB_USER")
-# password = os.environ.get("DB_PASSWORD")
-# host = os.environ.get("DB_HOST")
-# database = os.environ.get("DB_NAME")
-# port = int(os.environ.get("DB_PORT", "5432"))
 
 DATABASE_URL = os.environ.get("PG_DATABASE_URL")
 
